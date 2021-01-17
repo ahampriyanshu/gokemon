ad257803c0e76f47358bbc136abec52a63b5477e
Priyanshu Tiwari <ahampriyanshu@gmail.com>
2021-01-18 01:52:58 +0530

    just about to finish

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"math/rand"
 	"html/template"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"strconv"
 	"github.com/mtslzr/pokeapi-go"
 	"github.com/joho/godotenv"
 )
 	
 	var tpl = template.Must(template.ParseFiles("index.html"))
-	
-	func indexHandler(w http.ResponseWriter, r *http.Request) {
-		tpl.Execute(w, nil)
+
+	type Pokedata struct {
+		Query      string
+		Name      string
+		Experience  int
+		Height      int
+		Weight      int
+		Avatar      string
+		Type		string
+		// Abilities   []struct
 	}
 	
-	func searchHandler(w http.ResponseWriter, r *http.Request) {
+	func dataHandler(w http.ResponseWriter, r *http.Request) {
 		u, err := url.Parse(r.URL.String())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -28,13 +36,32 @@ import (
 	
 		params := u.Query()
 		searchQuery := params.Get("q")
+		searchQuery = strings.ToLower(searchQuery) 
 		if searchQuery == "" {
-			searchQuery =strconv.Itoa(rand.Intn(500 - 1) + 1) 
+			searchQuery = strconv.Itoa(rand.Intn(500 - 1) + 1) 
 		}
-		pokedata, err := pokeapi.Pokemon(searchQuery)
-		fmt.Println("Search Query is: ", pokedata)
 
+		pokejson, err := pokeapi.Pokemon(searchQuery)
 
+		pokedata := &Pokedata{
+			Query:        searchQuery,
+			Name:    	  strings.Title(pokejson.Name),
+			Experience:   pokejson.BaseExperience,
+			Height:       pokejson.Height,
+			Weight:       pokejson.Weight,
+			Avatar:  	  pokejson.Sprites.FrontDefault,
+			Type:		  pokejson.Types[0].Type.Name,
+			// Abilities:	  pokejson.Abilities[0],
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, pokedata)
+		if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+		}
+
+	buf.WriteTo(w)
 	}
 
 	func main() {
@@ -51,7 +78,7 @@ import (
 		fs := http.FileServer(http.Dir("assets"))
 		mux := http.NewServeMux()
 		mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-		mux.HandleFunc("/", indexHandler)
-		mux.HandleFunc("/search", searchHandler)
+		mux.HandleFunc("/", dataHandler)
+		mux.HandleFunc("/search", dataHandler)
 		http.ListenAndServe(":"+port, mux)
 	}
