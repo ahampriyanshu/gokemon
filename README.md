359b9cc070bd3de470ef8e5647257dda247dca0b
Priyanshu Tiwari <ahampriyanshu@gmail.com>
2021-01-17 15:25:42 +0530

    reinit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Math-is-fun
-
-This project will surely make me learn git , python and its essentail libraries
+# Gokemon
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+	
+	var tpl = template.Must(template.ParseFiles("index.html"))
+	
+	func indexHandler(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+	
+	func searchHandler(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+	
+		fmt.Println("Search Query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+	}
+
+	func main() {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Error loading .env file")
+		}
+	
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "3000"
+		}
+	
+
+		fs := http.FileServer(http.Dir("assets"))
+		mux := http.NewServeMux()
+		mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+		mux.HandleFunc("/", indexHandler)
+		mux.HandleFunc("/search", searchHandler)
+		http.ListenAndServe(":"+port, mux)
+	}
